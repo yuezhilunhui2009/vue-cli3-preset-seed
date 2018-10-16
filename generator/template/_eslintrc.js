935451bf72032e4d5cde1eafb3911b61df8dc438
yuezhilunhui2009 <yuezhilunhui2009@gmail.com>
2018-10-16 11:10:20 +0800

    chore(_eslintrc.js): 添加 eslint 配置文件

---
+++ generator/template/_eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  rules: {
+    indent: ['error', 2],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  },
+  root: true,
+  env: {
+    node: true
+  },
+  'extends': [
+    'plugin:vue/essential',
+    '@vue/standard'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
+}
