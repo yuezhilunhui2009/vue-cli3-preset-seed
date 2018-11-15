3977ce2dfcbeadac04b07ba90c1b4e16d943e175
yuezhilunhui2009 <yuezhilunhui2009@gmail.com>
2018-11-16 02:45:19 +0800

    docs: 添加开发笔记

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    title: 'vue-cli3-preset-seed 文档',
+    themeConfig: {
+        sidebar: 'auto',
+        repo: 'https://github.com/yuezhilunhui2009/vue-cli3-preset-seed',
+    }
+  }
